d3ddbfe85a3c3a6252efea021022eca52fd6e77d
Stephanie <Stephaniechloe21@gmail.com>
2023-08-25 15:31:42 -0700

    Comment

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
+# Create a file named school
 file { 'school':
-ensure  => file,
-path    => '/tmp/school',
-content => 'I love Puppet',
-owner   => 'www-data',
-group   => 'www-data',
-mode    => '0744'
+  ensure  => file,
+  path    => '/tmp/school',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744'
 }

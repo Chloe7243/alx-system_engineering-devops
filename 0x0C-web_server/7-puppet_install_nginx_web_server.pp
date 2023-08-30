395530d7c16aad50f418d1c983ef5cda3b0692a5
Stephanie <Stephaniechloe21@gmail.com>
2023-08-30 12:28:36 -0700

    tasks

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,41 @@
+$location = '/etc/nginx/sites-available/default'
+$file_url = 'https://raw.githubusercontent.com/Chloe7243/alx-system_engineering-devops/main/0x0C-web_server/default'
+
+# Run apt-get update
+exec { 'apt-update':
+  command => '/usr/bin/apt-get update'
+}
+
+# Install nginx
+package { 'nginx':
+  ensure  => installed,
+  require => Exec['apt-update'],
+}
+
+# Create a new index.html
+file { 'Create index.html':
+  require => Package['nginx'],
+  path    => '/var/www/html/index.html',
+  content => 'Hello World!\n'
+}
+
+# Create a new error page
+file { 'Create 404.html':
+  require => Package['nginx'],
+  path    => '/var/www/html/404.html',
+  content => 'Ceci n\'est pas une page\n'
+}
+
+# Replace default site config
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  require => Package['nginx']
+}-> exec { 'Replace config':
+  command => "/usr/bin/curl ${file_url} > ${location}"
+}
+
+# Start nginx service
+service { 'nginx':
+  ensure  => running,
+  require => Exec['Replace config'],
+}

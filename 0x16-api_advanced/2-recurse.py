1f4b38059bb71e1127c46d76246aeb35b0801f26
Stephanie <Stephaniechloe21@gmail.com>
2023-10-10 19:43:13 -0700

    task 4

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" 2. recurser """
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    sr = subreddit
+    res = requests.get(f"https://www.reddit.com/r/{sr}/hot.json?after={after}",
+                       headers={"User-Agent": "My-User-Agent"},
+                       allow_redirects=False)
+
+    if res.status_code >= 400:
+        return None
+
+    d = res.json()
+    result = hot_list + [p['data']['title'] for p in d['data']['children']]
+    if not d['data']['after']:
+        return result
+    return recurse(subreddit, result, d['data']['after'])

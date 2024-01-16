fd7fa68e23ab932f6604ba6724512abb351bc306
Stephanie <stephaniechloe21@gmail.com>
2024-01-16 14:51:27 -0800

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """ 0. How many subs? """
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers in a subreddit"""
-    import requests
     res = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json",
                        headers={"User-Agent": "My-User-Agent"},
                        allow_redirects=False)

c3bf0081affeddc2c1eb9d4ee0d95750458b7434
Stephanie <stephaniechloe21@gmail.com>
2024-01-18 02:36:24 -0800

    17

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,6 +5,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     res = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json",
-                       headers={"User-Agent": "Stephanies-alx-subs"},
+                       headers={"User-Agent": "My-User-Agent"},
                        allow_redirects=False)
+    print(res.status_code)
     return 0 if res.status_code != 200 else res.json()['data']['subscribers']
--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,61 +1,24 @@
 #!/usr/bin/python3
-""" Count it! """
-from requests import get
-
-REDDIT = "https://www.reddit.com/"
-HEADERS = {'user-agent': 'my-app/0.0.1'}
+""" 2. recurser """
+import requests
 
 
-def count_words(subreddit, word_list, after="", word_dic={}):
-    """
-    Returns a list containing the titles of all hot articles for a
-    given subreddit. If no results are found for the given subreddit,
-    the function should return None.
-    """
-    if not word_dic:
+def recurse(subreddit, word_list, titles={}, after=None):
+    """ recursion """
+    sr = subreddit
+    res = requests.get(f"https://www.reddit.com/r/{sr}/hot.json?after={after}",
+                       headers={"User-Agent": "My-User-Agent"},
+                       allow_redirects=False)
+
+    if res.status_code >= 400:
+        return None
+
+    d = res.json()
+    result = titles
+    for p in d['data']['children']:
         for word in word_list:
-            word_dic[word] = 0
-
-    if after is None:
-        word_list = [[key, value] for key, value in word_dic.items()]
-        word_list = sorted(word_list, key=lambda x: (-x[1], x[0]))
-        for w in word_list:
-            if w[1]:
-                print("{}: {}".format(w[0].lower(), w[1]))
-        return None
-
-    url = REDDIT + "r/{}/hot/.json".format(subreddit)
-
-    params = {
-        'limit': 100,
-        'after': after
-    }
-
-    r = get(url, headers=HEADERS, params=params, allow_redirects=False)
-
-    if r.status_code != 200:
-        return None
-
-    try:
-        js = r.json()
-
-    except ValueError:
-        return None
-
-    try:
-
-        data = js.get("data")
-        after = data.get("after")
-        children = data.get("children")
-        for child in children:
-            post = child.get("data")
-            title = post.get("title")
-            lower = [s.lower() for s in title.split(' ')]
-
-            for w in word_list:
-                word_dic[w] += lower.count(w.lower())
-
-    except:
-        return None
-
-    count_words(subreddit, word_list, after, word_dic)
+            if word in p['data']['title']:
+                result[word] +=
+    if not d['data']['after']:
+        return result.sort()
+    return recurse(subreddit, result, d['data']['after'])
